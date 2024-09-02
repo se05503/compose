a291ed5c6a71e38c26655ff643fcae8063c602bf
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-02 15:52:00 +0900

    [03. Button] step2: Text 앞에 아이콘을 추가한다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -4,23 +4,14 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Send
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -41,9 +32,14 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ButtonExample(onButtonClicked: () -> Unit) {
     Button(onClick = onButtonClicked) {
+        // contentDescription 의 경우, text 와 같은 의미를 가진다면 쓰지 않아도 된다. (null)
+        // 만약 text 와 다른 의미를 가진다면 해당 내용을 쓰는게 좋다.
+        Icon(
+            imageVector = Icons.Filled.Send,
+            contentDescription = null
+        )
         Text(text = "Send")
     }
-    // step1: Button 을 클릭했을 때 Toast 를 띄우기 -> Toast 메세지는 preview 에서는 확인할 수 없다.
 }
 
 @Preview(showBackground = true)
