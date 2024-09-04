aceaabfc3f8f6184daba7f07f72a5d8dcd98a960
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-04 14:07:59 +0900

    [06. Box - text] Box 내부에 Text 넣기 Text 를 여러개 넣어도 된다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,15 +3,12 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
@@ -22,7 +19,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             // setContent 안에 모든 코드를 집어넣는 것은 좋지 않다.
             ComposePracticeTheme {
-                Greeting("Android")
+                BoxEx()
             }
         }
     }
@@ -30,19 +27,10 @@ class MainActivity : ComponentActivity() {
 
 // 실제로 코드를 주로 작성하는 곳
 @Composable
-fun Greeting(text: String) {
-    Surface(
-        modifier = Modifier.padding(15.dp),
-        elevation = 5.dp,
-        border = BorderStroke(width = 2.dp, color = Color.Red),
-        shape = CircleShape,
-        color = MaterialTheme.colors.error
-    ) {
-        Text(
-            text = "Hello $text!",
-            modifier = Modifier.padding(8.dp)
-        )
-
+fun BoxEx() {
+    Box(modifier = Modifier.size(100.dp)) {
+        Text(text = "Compose", modifier = Modifier.align(Alignment.Center))
+        Text(text = "Coffee", modifier = Modifier.align(Alignment.BottomEnd))
     }
 }
 
@@ -50,6 +38,6 @@ fun Greeting(text: String) {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        Greeting(text = "세영")
+        BoxEx()
     }
 }
