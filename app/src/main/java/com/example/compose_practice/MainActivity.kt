40b356fb168dce4fc475ae4821411cb06befddbd
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-05 20:30:59 +0900

    [08. Column - text alignment] Column -> 개별 Text 는 Horizontal(1d) 로만 정렬 가능 Row -> 개별 Element 는 Vertical(1d) 로만 정렬 가능

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,17 +3,13 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
@@ -35,10 +31,16 @@ fun ColumnEx() {
     Column(
         modifier = Modifier.size(100.dp),
         horizontalAlignment = Alignment.End,
-        verticalArrangement = Arrangement.Bottom
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(text = "첫번째")
-        Text(text = "두번째")
+        Text(
+            text = "첫번째",
+            modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+        Text(
+            text = "두번째",
+            modifier = Modifier.align(Alignment.Start)
+        )
         Text(text = "세번째")
     }
 }
