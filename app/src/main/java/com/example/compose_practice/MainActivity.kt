713b01f5470cef9c59b7df1ebb7400c143b38aa6
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-05 20:12:08 +0900

    [07. Row -  icon] imageVector, contentDescription, modifier 속성을 사용할 수 있다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,14 +3,20 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
@@ -30,12 +36,33 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun RowEx() {
     Row(
-        modifier = Modifier.height(60.dp).width(250.dp),
+        modifier = Modifier
+            .height(50.dp)
+            .width(200.dp),
         verticalAlignment = Alignment.Bottom
     ) {
-        Text(text = "첫번째!", modifier = Modifier.align(Alignment.Top).weight(1f))
-        Text(text = "두번째!", modifier = Modifier.weight(2f))
-        Text(text = "세번째!", modifier = Modifier.weight(4f))
+        Text(
+            text = "첫번째!",
+            modifier = Modifier
+                .align(Alignment.Top)
+                .weight(3f)
+                .background(Color.Magenta),
+            textAlign = TextAlign.Center
+        )
+        Icon(
+            imageVector = Icons.Filled.Settings,
+            contentDescription = null,
+            modifier = Modifier
+                .weight(1f)
+                .background(Color.Cyan)
+        )
+        Text(
+            text = "세번째!",
+            modifier = Modifier
+                .weight(3f)
+                .background(Color.Blue),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
