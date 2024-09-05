424445695194aa6a200e7df198f31b61863f214b
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-05 19:34:10 +0900

    [07. Row -  align] - modifier 는 Row 에도 적용될 수 있음 - Text 가 가로로 위치함 - align = vertical (default) - 1D 에서는 Center 는 쓸 수 없다. -> CenterVertically 나 CenterHorizontally 만 쓸 수 있다. (Alignment.kt 확인하기)

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.height
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
@@ -21,24 +19,18 @@ class MainActivity : ComponentActivity() {
         setContent {
             // setContent 안에 모든 코드를 집어넣는 것은 좋지 않다.
             ComposePracticeTheme {
-                BoxEx()
+                RowEx()
             }
         }
     }
 }
 
-// 실제로 코드를 주로 작성하는 곳
 @Composable
-fun BoxEx() {
-    Box {
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Cyan)
-            .align(Alignment.CenterStart))
-        Box(modifier = Modifier
-            .size(70.dp)
-            .background(Color.Yellow)
-            .align(Alignment.BottomEnd))
+fun RowEx() {
+    Row(modifier = Modifier.height(40.dp)) {
+        Text(text = "첫번째!", modifier = Modifier.align(Alignment.Top))
+        Text(text = "두번째!", modifier = Modifier.align(Alignment.CenterVertically))
+        Text(text = "세번째!", modifier = Modifier.align(Alignment.Bottom))
     }
 }
 
@@ -46,6 +38,6 @@ fun BoxEx() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        BoxEx()
+        RowEx()
     }
 }
