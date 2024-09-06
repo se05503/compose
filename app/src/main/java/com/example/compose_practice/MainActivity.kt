fee8ce47ca991223c430d137460851b95dfc5075
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-06 14:39:02 +0900

    [09. BoxWithConstraints - widthIn, heightIn] width 와 height 의 min, max 값을 설정할 수 있다. 하지만 시스템 제약으로 원하는 값으로 설정되지 않는 경우도 있다. ex) Preview 자체의 max 값

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,7 +28,11 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Outer() {
     Column {
-        Inner()
+        Inner(
+            modifier = Modifier
+                .widthIn(min = 100.dp, max = 300.dp)
+                .heightIn(min = 20.dp, max = 40.dp)
+        )
     }
 }
 
