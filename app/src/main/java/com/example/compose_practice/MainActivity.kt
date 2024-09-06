6c86ad01ff599d97fcd958acefb6fee1064b1703
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-06 14:32:42 +0900

    [09. BoxWithConstraints - modifier] 1. maxWidth 값과 maxHeight 값이 정해져있다. 2. modifier: Modifier = Modifier -> Inner 함수에서 인자를 넣지 않았을 때의 기본값 설정

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,45 +3,36 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.size
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            // setContent 안에 모든 코드를 집어넣는 것은 좋지 않다.
             ComposePracticeTheme {
-                ColumnEx()
+                Outer()
             }
         }
     }
 }
 
 @Composable
-fun ColumnEx() {
-    Column(
-        modifier = Modifier.size(100.dp),
-        horizontalAlignment = Alignment.End,
-        verticalArrangement = Arrangement.Center
-    ) {
-        Text(
-            text = "첫번째",
-            modifier = Modifier.align(Alignment.CenterHorizontally)
-        )
-        Text(
-            text = "두번째",
-            modifier = Modifier.align(Alignment.Start)
-        )
-        Text(text = "세번째")
+fun Outer() {
+    Column {
+        Inner()
+    }
+}
+
+@Composable
+private fun Inner(modifier: Modifier = Modifier) {
+    BoxWithConstraints(modifier) {
+        Text("maxWidth: $maxWidth, maxHeight: $maxHeight, minWidth: $minWidth, minHeight: $minHeight")
     }
 }
 
@@ -49,6 +40,6 @@ fun ColumnEx() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        ColumnEx()
+        Outer()
     }
 }
