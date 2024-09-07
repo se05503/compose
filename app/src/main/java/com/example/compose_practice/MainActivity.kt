ace8e46f0fd87b040cb3e31a66167bae101f0aa6
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-07 12:16:41 +0900

    [13. CheckBox - 올바른 상태변화] - compose 는 언제든지 상태를 날릴 수 있음 - mutableStateOf 만 사용하면 compose 때문에 날아갈 가능성이 있기 때문에 꼭 remember 을 붙여줘야 한다. - remember : 상태를 기억했다가 나중에 다시 쓰게 하겠다! - value 프로퍼티를 사용해야 한다. - recomposition : 다시 rendering 되는 과정 - compose 에서는 다시 그려지는 과정이 반복됨 (recomposition). 상태가 바뀌었을 때 recomposition 이 일어남.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import coil.compose.AsyncImage
@@ -29,12 +31,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CheckBoxEx() {
     Row(verticalAlignment = Alignment.CenterVertically) {
-        var ischecked = false
-        Checkbox(checked = ischecked, onCheckedChange = {
-            ischecked = !ischecked
+        var isChecked = remember { mutableStateOf(false) }
+        Checkbox(checked = isChecked.value, onCheckedChange = {
+            isChecked.value = !isChecked.value
         })
         Text(text = "CheckBox")
     }
+
+    // 구조를 분해한다 = 비구조화, 반구조화 (destruction)
+    val (a,b) = listOf(2,3)
 }
 
 @Preview(showBackground = true)
