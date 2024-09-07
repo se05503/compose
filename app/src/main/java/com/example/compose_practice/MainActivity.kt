cd3b66825c690a433422a5eaa3da537d80dfd594
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-07 12:42:21 +0900

    [13. CheckBox - delegated properties] - 매번 value 프로퍼티를 쓰는게 귀찮을 수 있음 -> isChecked 가 isChecked.value 인 것처럼 사용할 수 있는 방법이 있음 - by 를 사용하여 속성 위임을 할 수 있음

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,18 +3,12 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Checkbox
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
-import coil.compose.AsyncImage
-import coil.compose.rememberImagePainter
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,15 +25,15 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CheckBoxEx() {
     Row(verticalAlignment = Alignment.CenterVertically) {
-        var isChecked = remember { mutableStateOf(false) }
-        Checkbox(checked = isChecked.value, onCheckedChange = {
-            isChecked.value = !isChecked.value
+        var isChecked by remember { mutableStateOf(false) }
+        Checkbox(checked = isChecked, onCheckedChange = {
+            isChecked = !isChecked
         })
         Text(text = "CheckBox")
     }
 
     // 구조를 분해한다 = 비구조화, 반구조화 (destruction)
-    val (a,b) = listOf(2,3)
+    val (a, b) = listOf(2, 3)
 }
 
 @Preview(showBackground = true)
