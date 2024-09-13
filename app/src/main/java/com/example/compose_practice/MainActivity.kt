e81a02b9bedd6e871e49628eb6183e67cd97b290
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-13 15:31:41 +0900

    [ConstraintSet] 방식) Box 의 Modifier 에 constrainAs 값을 일일히 설정하지 않고 ConstraintSet 안에 한번에 모아두기
    step 1) ConstraintSet 에다가 createRefFor 로 레퍼런스 만들기
    
    step 2) contrain 속성으로 후행 람다에 레이아웃 속성 지정하기
    
    step 3) ConstraintLayout 내부 각각의 Box 의 modifier 에 layoutId 로 레퍼런스 이름(createRefFor의 parameter) 지정하기
    
    step 4) ConstraintLayout 의 parameter 에 만든 ConstraintSet 변수 넣기 (연결)

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -12,9 +12,11 @@ import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    ConstraintLayoutEx()
+                    ConstraintSetEx()
                 }
             }
         }
@@ -34,38 +36,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun ConstraintLayoutEx() {
-    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
-        // destruction 분해
-        val (redBox, magentaBox, greenBox, yellowBox) = createRefs()
+fun ConstraintSetEx() {
+
+    val constraintSet = ConstraintSet {
+        val redBox = createRefFor("redBox")
+        val magentaBox = createRefFor("magentaBox")
+        val greenBox = createRefFor("greenBox")
+        val yellowBox = createRefFor("yellowBox")
+
+        constrain(redBox) {
+            bottom.linkTo(parent.bottom, margin = 12.dp)
+            end.linkTo(parent.end, margin = 4.dp)
+        }
+
+        constrain(magentaBox) {
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+        }
+
+        constrain(greenBox) {
+            centerTo(parent)
+        }
+
+        constrain(yellowBox) {
+            start.linkTo(magentaBox.end)
+            top.linkTo(magentaBox.bottom)
+        }
+    }
+
+    ConstraintLayout(
+        constraintSet,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
         Box(
             modifier = Modifier
                 .size(40.dp)
                 .background(Color.Red)
-                .constrainAs(redBox) {
-                    bottom.linkTo(parent.bottom, margin = 12.dp)
-                    end.linkTo(parent.end, margin = 4.dp)
-                }
+                .layoutId("redBox")
         )
         Box(
             modifier = Modifier
                 .size(40.dp)
                 .background(Color.Magenta)
-                .constrainAs(magentaBox) {
-                    start.linkTo(parent.start)
-                    end.linkTo(parent.end)
-                }
+                .layoutId("magentaBox")
         )
         Box(
             modifier = Modifier
                 .size(40.dp)
                 .background(Color.Green)
-                .constrainAs(greenBox) {
-                    centerTo(parent)
-                }
+                .layoutId("greenBox")
         )
         Box(
-            modifier = Modifier.size(40.dp).background(Color.Yellow)
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Yellow)
+                .layoutId("yellowBox")
         )
     }
 }
@@ -74,6 +100,6 @@ fun ConstraintLayoutEx() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        ConstraintLayoutEx()
+        ConstraintSetEx()
     }
 }
