025b8a52cd659eedfb2b64b4eeb85956ff7ad5e6
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-13 14:45:53 +0900

    [ConstraintLayout - createRefs, constrainAs, linkTo] - 의존성 추가 필요 - slotAPI 의 일종
    1. create Refs
    - createRef -> 한개의 Ref 리턴
    - createRefs -> 여러 개의 Ref 리턴
    - createRefs 문서를 살펴보면 ConstrainedLayoutReferences 에서 component1 ~ component16 까지 존재한다. -> 한번에 최대 16개의 레퍼런스를 가져올 수 있다. 만약 16개 이상이 필요하면 2번 이상 호출해야 한다.
    
    2. constrainAs
    - box 가 어디에 위치해야 할 지 렌더링을 하는 modifier
    - 후행 람다가 필요함
    
    3. linkTo
    parameter : anchor(어디에 붙일 것인지), margin(여백)

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,21 +3,18 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Card
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
+import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil.compose.AsyncImage
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,77 +22,61 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                // Surface 의 modifier, color 속성은 영향이 없음 (넣은 이유는 설명안하심)
-                androidx.compose.material.Surface(
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Column {
-                        PracticeChap12(cardData)
-                        PracticeChap12(cardData)
-                    }
+                    ConstraintLayoutEx()
                 }
             }
         }
     }
-    companion object {
-        val cardData = CardData(
-            imageUri = "https://cdn.pixabay.com/photo/2015/04/23/22/00/flowers-736885_960_720.jpg",
-            imageDescription = "example network image",
-            author = "Dalinaum",
-            description = "엔텔로프 캐년은 죽기 전에 꼭 봐야할 절경으로 소개되었습니다."
-        )
-    }
 }
 
 @Composable
-fun PracticeChap12(cardData: CardData) {
-    val placeHolderColor = Color(0x28000000)
-    Card(
-        modifier = Modifier
-            .padding(12.dp)
-    ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(20.dp)
-        ) {
-            AsyncImage(
-                model = cardData.imageUri,
-                contentDescription = cardData.imageDescription,
-                modifier = Modifier
-                    .size(52.dp) // 이게 둥글게 하는데 핵심 코드
-                    .clip(CircleShape), // RoundedCornerShape 도 괜찮음
-                // Crop -> 사이즈에 맞지 않는건 잘라냄
-                contentScale = ContentScale.Crop,
-                // placeholder 는 이미지가 없을 때 보이는 것
-                placeholder = ColorPainter(placeHolderColor)
-            )
-            Spacer(modifier = Modifier.size(20.dp))
-            Column {
-                Text(text = cardData.author)
-                Spacer(modifier = Modifier.size(4.dp))
-                Text(
-                    text = cardData.description
-                )
-            }
-        }
+fun ConstraintLayoutEx() {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        // destruction 분해
+        val (redBox, magentaBox, greenBox, yellowBox) = createRefs()
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Red)
+                .constrainAs(redBox) {
+                    bottom.linkTo(parent.bottom, margin = 12.dp)
+                    end.linkTo(parent.end, margin = 4.dp)
+                }
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Magenta)
+                .constrainAs(magentaBox) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                }
+        )
+        Box(
+            modifier = Modifier
+                .size(40.dp)
+                .background(Color.Green)
+                .constrainAs(greenBox) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                    bottom.linkTo(parent.bottom)
+                }
+        )
+        Box(
+            modifier = Modifier.size(40.dp).background(Color.Yellow)
+        )
     }
-
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        Row {
-            PracticeChap12(MainActivity.cardData)
-        }
+        ConstraintLayoutEx()
     }
 }
-
-data class CardData(
-    val imageUri: String,
-    val imageDescription: String,
-    val author: String,
-    val description: String
-)
