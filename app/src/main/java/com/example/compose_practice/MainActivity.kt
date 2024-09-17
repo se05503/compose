fb8784288a7a3bbca59197d2450be62b9cb5e50a
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-17 21:43:15 +0900

    [카탈로그 앱 구현 - 스스로 하기&catalog2] - 아래 사진을 정사각형 모양으로 배치 + 스크롤링 하고 싶은데 잘 모르겠음

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -31,9 +32,10 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
     companion object {
         val catalogData = CatalogData(
-            imageUri = "https://media.istockphoto.com/id/139717376/ko/%EC%82%AC%EC%A7%84/%EB%A1%A4%EB%9F%AC%EC%BD%94%EC%8A%A4%ED%84%B0.jpg?s=2048x2048&w=is&k=20&c=jaY_ApFfzncvTAoVmuC-uHZVC7oJKAhJcgTb2B8ir9M=",
+            imageUri = listOf("https://media.istockphoto.com/id/139717376/ko/%EC%82%AC%EC%A7%84/%EB%A1%A4%EB%9F%AC%EC%BD%94%EC%8A%A4%ED%84%B0.jpg?s=2048x2048&w=is&k=20&c=jaY_ApFfzncvTAoVmuC-uHZVC7oJKAhJcgTb2B8ir9M=","https://media.istockphoto.com/id/104144229/ko/%EC%82%AC%EC%A7%84/%EA%B7%B8%EB%9E%9C%EB%93%9C-%EC%BA%90%EB%85%84-%ED%92%8D%EA%B2%BD-at-dusk-%EC%8B%9C%EC%B2%AD%EC%88%9C-%EB%A9%94%ED%8A%B8%EB%A1%9C%ED%8F%B4%EB%A6%AC%EC%8A%A4-%EC%82%AC%EB%A7%89.jpg?s=612x612&w=0&k=20&c=RrNLszZZYSCih6NiQ7n6PtZ1JxeZsI1D4DAklrUHX7s="),
             imageDescription = "카탈로그 실습 구현용 이미지",
             title = "해변 놀이 공원",
             content = "놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다."
@@ -43,19 +45,35 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun CatalogEx(catalogData: CatalogData) {
-    Card(modifier = Modifier.padding(20.dp)) {
+    Column(modifier = Modifier.padding(20.dp)) {
+        Card {
             Column(modifier = Modifier.padding(20.dp)) {
-            AsyncImage(model = catalogData.imageUri, contentDescription =catalogData.imageDescription)
-            Spacer(modifier = Modifier.size(12.dp))
-            Text(text =catalogData.title, fontSize = 40.sp)
-            Spacer(modifier = Modifier.size(12.dp))
-            Text(text =catalogData.content)
+                AsyncImage(
+                    model = catalogData.imageUri[0],
+                    contentDescription = catalogData.imageDescription
+                )
+                Spacer(modifier = Modifier.size(12.dp))
+                Text(text = catalogData.title, fontSize = 40.sp)
+                Spacer(modifier = Modifier.size(12.dp))
+                Text(text = catalogData.content)
+            }
+        }
+        Spacer(modifier = Modifier.size(20.dp))
+        Card {
+            Column(modifier = Modifier.padding(20.dp)) {
+                AsyncImage(
+                    model = catalogData.imageUri[1],
+                    contentDescription = catalogData.imageDescription,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
 
 data class CatalogData(
-    val imageUri: String,
+    val imageUri: List<String>,
     val imageDescription: String,
     val title: String,
     val content: String
