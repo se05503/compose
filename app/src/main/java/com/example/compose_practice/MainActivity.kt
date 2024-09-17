5348fd54c24985a6cade7f2e96597ae3fb304dbf
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-17 21:19:34 +0900

    [카탈로그 앱 구현 - 스스로 하기&catalog1] - Text 에서 marginTop 을 설정하는 방법? -> 몰라서 일단 Spacer 넣음

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -4,9 +4,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -16,9 +15,11 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import coil.compose.AsyncImage
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,63 +27,44 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    ConstraintLayoutEx()
-                }
+                CatalogEx(catalogData)
             }
         }
+    }
+    companion object {
+        val catalogData = CatalogData(
+            imageUri = "https://media.istockphoto.com/id/139717376/ko/%EC%82%AC%EC%A7%84/%EB%A1%A4%EB%9F%AC%EC%BD%94%EC%8A%A4%ED%84%B0.jpg?s=2048x2048&w=is&k=20&c=jaY_ApFfzncvTAoVmuC-uHZVC7oJKAhJcgTb2B8ir9M=",
+            imageDescription = "카탈로그 실습 구현용 이미지",
+            title = "해변 놀이 공원",
+            content = "놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다."
+        )
     }
 }
 
 @Composable
-fun ConstraintLayoutEx() {
-    ConstraintLayout(Modifier.fillMaxSize()) {
-        val (redBox, yellowBox, magentaBox, text) = createRefs()
-
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Red)
-                .constrainAs(redBox) {
-                    // 위치 제약을 설정하는 곳 (기본값: 왼쪽 상단)
-                    start.linkTo(parent.start, margin = 10.dp)
-                }
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Yellow)
-                .constrainAs(yellowBox) {
-                    start.linkTo(parent.start, margin = 100.dp)
-                }
-        )
-        Box(
-            modifier = Modifier
-                .size(40.dp)
-                .background(Color.Magenta)
-                .constrainAs(magentaBox) {
-                    start.linkTo(parent.start, margin = 30.dp)
-                }
-        )
-        createVerticalChain(redBox,yellowBox,magentaBox, chainStyle = ChainStyle.SpreadInside)
-//        val barrier = createBottomBarrier(redBox,yellowBox,magentaBox)
-        val barrier = createEndBarrier(redBox,yellowBox,magentaBox)
-        Text(
-            text = "Barrier",
-            modifier = Modifier.constrainAs(text) {
-                start.linkTo(barrier)
-                bottom.linkTo(yellowBox.bottom)
-            })
+fun CatalogEx(catalogData: CatalogData) {
+    Card(modifier = Modifier.padding(20.dp)) {
+            Column(modifier = Modifier.padding(20.dp)) {
+            AsyncImage(model = catalogData.imageUri, contentDescription =catalogData.imageDescription)
+            Spacer(modifier = Modifier.size(12.dp))
+            Text(text =catalogData.title, fontSize = 40.sp)
+            Spacer(modifier = Modifier.size(12.dp))
+            Text(text =catalogData.content)
+        }
     }
 }
+
+data class CatalogData(
+    val imageUri: String,
+    val imageDescription: String,
+    val title: String,
+    val content: String
+)
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        ConstraintLayoutEx()
+        CatalogEx(MainActivity.catalogData)
     }
 }
