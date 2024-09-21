a5fe2c233a45a53f7d239f94142d5f1f4c02652a
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-21 10:26:36 +0900

    [Dialog] AlertDialog 1. onDismissRequest 2. confirmButton 3. dismissButton 4. title 5. text
    - Interactive Mode 에서 사용가능하다.
    - xml 보다 dialog 를 만드는 방식이 훨씬 편해진 것 같다.
    -> xml 에서 dialog 만드는 과정은 생각보다 쉽지 않았다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -6,11 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.Card
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
@@ -27,11 +24,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                Surface(
+                Surface( // Surface 의 역할이 정확히 뭔지 잘 모르겠음
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    CanvasEx()
+                    DialogEx()
                 }
             }
         }
@@ -40,39 +37,47 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun CanvasEx() {
-    /*
-    Canvas 는 무언가 그릴 수 있는 공간
-    Modifier 를 반드시 설정해야 한다 (단점)
-    핸드폰에서는 매우 작게 뜨고, Preview 에서만 뜬다.
-    y 값이 커질수록 아래로 가고, y 값이 작아질수록 위로 간다.
-    */
-    Canvas(modifier = Modifier.size(20.dp)) {
-        // 시작(Offset)과 끝(Offset)의 x,y 좌표 지정
-        drawLine(Color.Red, Offset(30f, 10f), Offset(50f,10f))
+fun DialogEx() {
+    var openDialog by remember { mutableStateOf(false) } // 초기값: false
+    var counter by remember { mutableStateOf(0) } // 초기값: 0
 
-        // 매개변수: 색상, 반지름, 중앙 위치(Offset)
-        drawCircle(Color.Yellow, 10f, Offset(15f, 30f))
+    Column {
+        Button(onClick = { openDialog = !openDialog }) { // openDialog = !openDialog 말고도 openDialog = true 도 같은 표현이다.
+            Text("Open Dialog")
+        }
+        Text("카운터: $counter")
+    }
 
-        // Offset(위치)
-        drawRect(Color.Green, Offset(30f,40f), Size(10f, 10f))
-
-        /*
-         drawLine 을 활용
-         moveTo(2.01f, 21.0f) : 처음 위치
-         lineTo(23.0f, 12.0f) : 이쪽으로 이동
-         lineTo(2.01f, 3.0f)
-         lineTo(2.0f, 10.0f)
-         lineToRelative(15.0f, 2.0f) : 해당 거리만큼 이동
-         lineToRelative(-15.0f, 2.0f)
-         close() -> 처음 위치 이동
-         */
-        drawLine(Color.Blue, Offset(2.01f, 21.0f), Offset(23.0f, 12.0f))
-        drawLine(Color.Blue, Offset(23.0f, 12.0f), Offset(2.01f, 3.0f))
-        drawLine(Color.Blue, Offset(2.01f, 3.0f), Offset(2.0f, 10.0f))
-        drawLine(Color.Blue, Offset(2.0f, 10.0f), Offset(17.0f, 12.0f))
-        drawLine(Color.Blue, Offset(17.0f, 12.0f), Offset(2.0f, 14.0f))
-        drawLine(Color.Blue, Offset(2.0f, 14.0f), Offset(2.01f, 21.0f))
+    if(openDialog) {
+        // openDialog = true 일 때만 다이얼로그가 뜬다.
+        AlertDialog(onDismissRequest = {
+            // 다이얼로그 바깥쪽을 클릭했을 떄의 이벤트 -> 다이얼로그를 안띄운다.
+            openDialog = false
+        },
+        confirmButton = {
+            // 오른쪽 버튼
+            Button(onClick = {
+                counter++ // 카운터 증가
+                openDialog = false // 다이얼로그 종료
+            }) {
+                Text("Counter +1")
+            }
+        },
+        dismissButton = {
+            Button(onClick = {
+                openDialog = false // 다이얼로그 종료
+            }) {
+                Text("Cancel")
+            }
+        },
+        title = {
+            // dialog's title
+            Text(text = "Add Counter Dialog")
+        },
+        text = {
+            // dialog's description
+            Text("This is a sample dialog. \nWhen you click the confirm button(on the right), the counter increases by 1.")
+        })
     }
 }
 
@@ -80,6 +85,6 @@ fun CanvasEx() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        CanvasEx()
+        DialogEx()
     }
 }
