ea731a010fd031adc115445cb07f53ca85def0e5
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-23 13:10:17 +0900

    [SnackBar] - SnackBar 는 일정 시간이 지나면 사라지는 UI 기 때문에 coroutine 을 사용한다. 따라서 SnackBar 는 coroutineScope.launch 내에서 사용해줘야한다. - SnackBar를 직접 쓰지 않고 scaffoldState.snackbarHostState 를 사용해서 써야 한다. - coroutineScope.launch 의 범위 -> LaunchedEffect 안쪽이나 composition 밖에서만 사용할 수 있다   - LaunchEffect: composable 이 생성될 때 호출되는 api -> 처음에 한번만 호출되고 이후엔 key 값이 바뀌지 않으면 호출되지 않는다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -20,17 +20,18 @@ import androidx.compose.ui.unit.sp
 import androidx.compose.ui.window.Dialog
 import coil.compose.AsyncImage
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                Surface( // Surface 의 역할이 정확히 뭔지 잘 모르겠음
+                Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    DropDownMenuEx()
+                    SnackBarEx()
                 }
             }
         }
@@ -39,23 +40,72 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun DropDownMenuEx() {
-    var expandDropDownMenu by remember { mutableStateOf(false) }
+fun SnackBarEx() {
+
+    // step3
+    // @Composable layer 에서 쓰기 때문에 rememberCoroutineScope 를 만들어서 쓰는 것이 좋다.
+    // coroutineScope 를 만드는 이유는 Snackbar 가 suspend fun 이기 때문이다.
+    // suspend fun 은 coroutine 범위 내에서만 호출할 수 있는 함수이다.
+    val coroutineScope = rememberCoroutineScope()
+
+    // step1
+    // rememberScaffoldState() 를 사용하여 scaffoldState 를 만듭니다.
+    // 만든 scaffoldState 를 가지고 Scaffold 의 인자에 설정합니다.
     var counter by remember { mutableStateOf(0) }
+    val scaffoldState = rememberScaffoldState()
 
-    Column {
-        Button(onClick = { expandDropDownMenu = true }) {
-            Text(text = "Open DropDownMenu")
-        }
-        Text(text = "Counter: $counter")
-    }
+    /*
+    - rememberScaffoldState 에 snackbarHostState 를 지정하는 경우
+    val snackbarHostState = remember {SnackbarHostState()}
+    val scaffoldState = rememberScaffoldState(snackbarHostState = snackbarHostState)
+     */
+    Scaffold(scaffoldState = scaffoldState){
 
-    DropdownMenu(expanded = expandDropDownMenu, onDismissRequest = { expandDropDownMenu = false }) {
-        DropdownMenuItem(onClick = { counter++ }) {
-            Text(text = "Counter Increase")
+        /*
+        // composable 안에서는 coroutineScope 를 호출하려면 LaunchedEffect 를 감싸줘야 한다.
+        LaunchedEffect(scaffoldState.snackbarHostState) {
+            // key = scaffoldState.snackbarHostState
+            // key 값이 바뀌기 전에는 다시 호출되지 않는다.
+            // LaunchedEffect 는 처음 뜰 때만 호출하고, 상태가 바뀌기 전에는 호출되지 않는다.
+            coroutineScope.launch {
+                // Snackbar 는 suspend fun 이기 때문에 coroutine 범위 내에서만 호출할 수 있다.
+                // snackbarHostState 를 통해서 snackbar를 만들 수 있다.
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = "Counter: $counter",
+                    actionLabel = "Cancel",
+                    duration = SnackbarDuration.Short
+                )
+            }
         }
-        DropdownMenuItem(onClick = { counter-- }) {
-            Text(text = "Counter decrease")
+        */
+
+        Button(onClick = {
+            counter++
+            // onClick 은 composable 안이 아니기 때문에 괜찮다.
+            coroutineScope.launch {
+                // Snackbar 는 suspend fun 이기 때문에 coroutine 범위 내에서만 호출할 수 있다.
+                // snackbarHostState 를 통해서 snackbar를 만들 수 있다.
+                scaffoldState.snackbarHostState.showSnackbar(
+                    message = "Counter: $counter",
+                    actionLabel = "Cancel",
+                    duration = SnackbarDuration.Short
+                )
+
+                /*
+                - 결과값 변수를 설정하고 응답값에 따라 처리를 다르게 하고 싶은 경우
+                val result = scaffoldState.snackbarHostState.showSnackbar(
+                    message = "Counter: $counter",
+                    actionLabel = "Cancel",
+                    duration = SnackbarDuration.Short
+                )
+                when(result) {
+                    SnackbarResult.ActionPerformed -> {}
+                    SnackbarResult.Dismissed -> {}
+                }
+                 */
+            }
+        }) {
+            Text("Add")
         }
     }
 }
@@ -64,6 +114,6 @@ fun DropDownMenuEx() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        DropDownMenuEx()
+        SnackBarEx()
     }
 }
