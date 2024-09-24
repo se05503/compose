5e1902ab9eda12a0df2a4512ed0d039cfca602a8
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-24 13:46:12 +0900

    [카탈로그 앱 구현 - 해설 리팩토링 1회차] 해설 강의를 봤었는데, 며칠 뒤에 강의를 보지 않고 구조화한 필기를 기반으로 만든 코드입니다. 다시 해설 강의를 참조하여 리팩토링하기.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,12 +3,17 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -20,61 +25,74 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                CatalogEx(catalogData)
+                CatalogEx(itemList = itemDatas)
             }
         }
     }
+}
 
-    companion object {
-        val catalogData = CatalogData(
-            imageUri = listOf("https://media.istockphoto.com/id/139717376/ko/%EC%82%AC%EC%A7%84/%EB%A1%A4%EB%9F%AC%EC%BD%94%EC%8A%A4%ED%84%B0.jpg?s=2048x2048&w=is&k=20&c=jaY_ApFfzncvTAoVmuC-uHZVC7oJKAhJcgTb2B8ir9M=","https://media.istockphoto.com/id/104144229/ko/%EC%82%AC%EC%A7%84/%EA%B7%B8%EB%9E%9C%EB%93%9C-%EC%BA%90%EB%85%84-%ED%92%8D%EA%B2%BD-at-dusk-%EC%8B%9C%EC%B2%AD%EC%88%9C-%EB%A9%94%ED%8A%B8%EB%A1%9C%ED%8F%B4%EB%A6%AC%EC%8A%A4-%EC%82%AC%EB%A7%89.jpg?s=612x612&w=0&k=20&c=RrNLszZZYSCih6NiQ7n6PtZ1JxeZsI1D4DAklrUHX7s="),
-            imageDescription = "카탈로그 실습 구현용 이미지",
-            title = "해변 놀이 공원",
-            content = "놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다."
+@Composable
+fun Item(ItemData: ItemData) {
+    Card(elevation = 10.dp) {
+        Column(modifier = Modifier.padding(20.dp)) {
+            Image(
+                painter = painterResource(id = ItemData.id),
+                contentDescription = ItemData.title)
+            Spacer(modifier = Modifier.size(12.dp))
+            Text(text = ItemData.title, fontSize = 40.sp, style = TextStyle.Default)
+            Spacer(modifier = Modifier.size(12.dp))
+            Text(text = ItemData.description)
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ItemPreview() {
+    ComposePracticeTheme {
+        Item(
+            ItemData(
+                id = R.drawable.a1,
+                title = "해변 놀이 공원",
+                description = "놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다."
+            )
         )
     }
 }
 
 @Composable
-fun CatalogEx(catalogData: CatalogData) {
-    Column(modifier = Modifier.padding(20.dp)) {
-        Card {
-            Column(modifier = Modifier.padding(20.dp)) {
-                AsyncImage(
-                    model = catalogData.imageUri[0],
-                    contentDescription = catalogData.imageDescription
-                )
-                Spacer(modifier = Modifier.size(12.dp))
-                Text(text = catalogData.title, fontSize = 40.sp)
-                Spacer(modifier = Modifier.size(12.dp))
-                Text(text = catalogData.content)
-            }
-        }
-        Spacer(modifier = Modifier.size(20.dp))
-        Card {
-            Column(modifier = Modifier.padding(20.dp)) {
-                AsyncImage(
-                    model = catalogData.imageUri[1],
-                    contentDescription = catalogData.imageDescription,
-                    modifier = Modifier.fillMaxSize(),
-                    contentScale = ContentScale.Crop
-                )
-            }
+fun CatalogEx(itemList: List<ItemData>) {
+    LazyColumn {
+        items(itemList) { data ->
+            Item(data)
         }
     }
 }
-
-data class CatalogData(
-    val imageUri: List<String>,
-    val imageDescription: String,
-    val title: String,
-    val content: String
-)
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        CatalogEx(MainActivity.catalogData)
+        CatalogEx(itemList = itemDatas)
     }
 }
+
+val itemDatas : List<ItemData> = listOf(
+    ItemData(R.drawable.a1,"첫번째 이미지", "첫번째 이미지 설명"),
+    ItemData(R.drawable.a2,"두번째 이미지","두번째 이미지 설명"),
+    ItemData(R.drawable.a3,"세번째 이미지","세번째 이미지 설명"),
+    ItemData(R.drawable.a4,"네번째 이미지","네번째 이미지 설명"),
+    ItemData(R.drawable.a5,"다섯번째 이미지","다섯번째 이미지 설명"),
+    ItemData(R.drawable.a6,"여섯번째 이미지","여섯번째 이미지 설명"),
+    ItemData(R.drawable.a7,"일곱번째 이미지","일곱번째 이미지 설명"),
+    ItemData(R.drawable.a8,"여덟번째 이미지","여덟번째 이미지 설명"),
+    ItemData(R.drawable.a9,"아홉번째 이미지","아홉번째 이미지 설명")
+)
+
+
+
+data class ItemData(
+    val id: Int,
+    val title: String,
+    val description: String
+)
