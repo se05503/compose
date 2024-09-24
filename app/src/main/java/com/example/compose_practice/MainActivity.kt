cae29ffd7ad1a95a7bd55466feaba88a7dee96ba
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-24 15:33:31 +0900

    [State & State Hoisting] 첫번째 방법) rememberSaveable 사용 OutlinedTextField 를 사용하는 방법 -> 좋지 못한 방법 이 방법을 보완하려면 State Hoisting 이 필요함

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,24 +3,18 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.material.Card
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    CatalogEx(itemList = itemDatas)
+                    PyeongToSquareMeter()
                 }
             }
         }
@@ -41,51 +35,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Item(ItemData: ItemData) {
-    Card(
-        elevation = 10.dp,
-        modifier = Modifier.padding(10.dp) // padding 속성이 없으면 공간이 없어 elevation(그림자 효과)이 보이지 않는다.
-    ) {
-        Column(
-            modifier = Modifier.padding(20.dp)
-        ) {
-            Image(
-                painter = painterResource(id = ItemData.id),
-                contentDescription = ItemData.title) // 접근성을 위해 설정
-            Spacer(modifier = Modifier.size(12.dp))
-            Text(text = ItemData.title, fontSize = 40.sp, style = MaterialTheme.typography.h4)
-            Spacer(modifier = Modifier.size(12.dp))
-            Text(text = ItemData.description)
-        }
-    }
-}
+fun PyeongToSquareMeter() {
+    var pyeong by rememberSaveable { mutableStateOf("23") }
+    var squaremeter by rememberSaveable { mutableStateOf((23 * 3.306).toString()) }
 
-@Preview(showBackground = true)
-@Composable
-fun ItemPreview() {
-    ComposePracticeTheme {
-        Item(
-            ItemData(
-                id = R.drawable.a1, // drawable.a1 -> ?
-                title = "해변 놀이 공원",
-                description = "놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다."
-            )
+    Column(modifier = Modifier.padding(16.dp)) {
+        /*
+        jetpack compose 는 상태가 바뀌지 않으면 업데이트가 되지 않는다.
+        따라서 값이 수정될려면 상태가 바뀌어야한다.
+        configuration 이 바뀌는 대표적인 경우 = rotation (회전)
+        저장공간의 한계로 모든 값을 rememberSaveable 로 하는 것은 좋지 않다. (변수가 2개 정도면 괜찮음)
+        remember 와 rememberSaveable 만 근본적인 method 이고 나머지는 utility 이다.
+         */
+        OutlinedTextField(
+            value = pyeong,
+            onValueChange = {
+                if(it.isBlank()) {
+                    pyeong = ""
+                    squaremeter = ""
+                    return@OutlinedTextField // 이게 무슨 의미지?
+                } else {
+                    // float(실수)으로 바꿀 수 없는 것은 null 로 return 하겠다. null 이면 강제종료하겠다.
+                    val numericValue = it.toFloatOrNull() ?: return@OutlinedTextField
+                    pyeong = numericValue.toString()
+                    squaremeter = (numericValue*3.306).toString()
+                }
+            },
+            label = {
+                Text("평")
+            }
         )
-    }
-}
-
-@Composable
-fun CatalogEx(itemList: List<ItemData>) {
-    // Lazy : 필요할 때만 보여주겠다.
-    // Column : 세로 방향
-    LazyColumn {
-//         수동으로 한개를 넣는 경우 -> items 대신 item 사용하면 됨
-//        item {
-//            Item(itemList[5])
-//        }
-        items(itemList) { data ->
-            Item(data)
-        }
+        OutlinedTextField(
+            value = squaremeter,
+            onValueChange = {},
+            label = {
+                Text("제곱미터")
+            }
+        )
     }
 }
 
@@ -93,26 +79,6 @@ fun CatalogEx(itemList: List<ItemData>) {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        CatalogEx(itemList = itemDatas)
+        PyeongToSquareMeter()
     }
 }
-
-val itemDatas = listOf(
-    ItemData(R.drawable.a1,"첫번째 이미지", "첫번째 이미지 설명"),
-    ItemData(R.drawable.a2,"두번째 이미지","두번째 이미지 설명"),
-    ItemData(R.drawable.a3,"세번째 이미지","세번째 이미지 설명"),
-    ItemData(R.drawable.a4,"네번째 이미지","네번째 이미지 설명"),
-    ItemData(R.drawable.a5,"다섯번째 이미지","다섯번째 이미지 설명"),
-    ItemData(R.drawable.a6,"여섯번째 이미지","여섯번째 이미지 설명"),
-    ItemData(R.drawable.a7,"일곱번째 이미지","일곱번째 이미지 설명"),
-    ItemData(R.drawable.a8,"여덟번째 이미지","여덟번째 이미지 설명"),
-    ItemData(R.drawable.a9,"아홉번째 이미지","아홉번째 이미지 설명")
-)
-
-
-
-data class ItemData(
-    @DrawableRes val id: Int, // @DrawableRes -> ?
-    val title: String,
-    val description: String
-)
