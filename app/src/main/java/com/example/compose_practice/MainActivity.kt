1fd6a0980b7a708bddaea6df6302c67105f6dcb5
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-24 12:03:18 +0900

    [카탈로그 앱 구현 - 스스로 하기 최종본] commit 으로 비교하기 위해 한번 더 커밋을 합니다 (git commit 의 다른 방식을 이용해보고 싶지만, 어떻게 하는지 잘 모르는 상태에서 이전에 해당 커밋에 대해 무턱대고 drop commit 을 적용했다가 잘 안돼서 한번 더 커밋을 남깁니다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,98 +3,78 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
-import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    BottomAppBarEx()
-                }
+                CatalogEx(catalogData)
             }
         }
     }
 
+    companion object {
+        val catalogData = CatalogData(
+            imageUri = listOf("https://media.istockphoto.com/id/139717376/ko/%EC%82%AC%EC%A7%84/%EB%A1%A4%EB%9F%AC%EC%BD%94%EC%8A%A4%ED%84%B0.jpg?s=2048x2048&w=is&k=20&c=jaY_ApFfzncvTAoVmuC-uHZVC7oJKAhJcgTb2B8ir9M=","https://media.istockphoto.com/id/104144229/ko/%EC%82%AC%EC%A7%84/%EA%B7%B8%EB%9E%9C%EB%93%9C-%EC%BA%90%EB%85%84-%ED%92%8D%EA%B2%BD-at-dusk-%EC%8B%9C%EC%B2%AD%EC%88%9C-%EB%A9%94%ED%8A%B8%EB%A1%9C%ED%8F%B4%EB%A6%AC%EC%8A%A4-%EC%82%AC%EB%A7%89.jpg?s=612x612&w=0&k=20&c=RrNLszZZYSCih6NiQ7n6PtZ1JxeZsI1D4DAklrUHX7s="),
+            imageDescription = "카탈로그 실습 구현용 이미지",
+            title = "해변 놀이 공원",
+            content = "놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다. 놀이 공원 설명입니다."
+        )
+    }
 }
 
 @Composable
-fun BottomAppBarEx() {
-    val scaffoldState = rememberScaffoldState()
-    val coroutineScope = rememberCoroutineScope()
-    var counter by remember {
-        mutableStateOf(0)
-    }
-
-    Scaffold(
-        scaffoldState = scaffoldState,
-        bottomBar = {
-            BottomAppBar {
-                Text("BottomBar")
-                Spacer(modifier = Modifier.size(4.dp))
-                Button(onClick = {
-                    // composable 내부가 아니기 때문에 바로 coroutineScope.launch 를 쓸 수 있다. (LaunchedEffect 를 쓰지 않아도 됨)
-                    coroutineScope.launch {
-                        scaffoldState.snackbarHostState.showSnackbar(
-                            "SnackBar!"
-                        )
-                    }
-                }) {
-                    Text("Button")
-                }
-                Spacer(modifier = Modifier.size(4.dp))
-                Button(onClick = {
-                    counter++
-                    coroutineScope.launch {
-                        scaffoldState.snackbarHostState.showSnackbar(
-                            "counter: $counter"
-                        )
-                    }
-                }) {
-                    Text("Increase")
-                }
-                Spacer(modifier = Modifier.size(4.dp))
-                Button(onClick = {
-                    counter--
-                    coroutineScope.launch {
-                        scaffoldState.snackbarHostState.showSnackbar(
-                            "counter: $counter"
-                        )
-                    }
-                }) {
-                    Text("Decrease")
-                }
+fun CatalogEx(catalogData: CatalogData) {
+    Column(modifier = Modifier.padding(20.dp)) {
+        Card {
+            Column(modifier = Modifier.padding(20.dp)) {
+                AsyncImage(
+                    model = catalogData.imageUri[0],
+                    contentDescription = catalogData.imageDescription
+                )
+                Spacer(modifier = Modifier.size(12.dp))
+                Text(text = catalogData.title, fontSize = 40.sp)
+                Spacer(modifier = Modifier.size(12.dp))
+                Text(text = catalogData.content)
             }
         }
-    ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Text(
-                text = "counter: $counter",
-                modifier = Modifier.align(Alignment.Center)
-            )
+        Spacer(modifier = Modifier.size(20.dp))
+        Card {
+            Column(modifier = Modifier.padding(20.dp)) {
+                AsyncImage(
+                    model = catalogData.imageUri[1],
+                    contentDescription = catalogData.imageDescription,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop
+                )
+            }
         }
     }
 }
+
+data class CatalogData(
+    val imageUri: List<String>,
+    val imageDescription: String,
+    val title: String,
+    val content: String
+)
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        BottomAppBarEx()
+        CatalogEx(MainActivity.catalogData)
     }
 }
