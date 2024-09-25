01b8c42ab1b34956d5906fea66f1db1ca6d72456
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-25 15:21:16 +0900

    [ConstraintLayout 활용 - 스스로 해보기] 하면서 느끼는 점 : 계층 관계가 파악하는게 어렵다.
    Card 넣기 전에는 constraintLayout 이 원하는대로 적용되었는데, Card 를 ConstraintLayout 내부에 넣고나서 레이아웃이 망가졌다.
    
    시도방법1: Card reference 생성하기 -> 적용 X

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,18 +3,23 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.constraintlayout.compose.ConstraintLayout
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,63 +27,74 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                // Surface 로 감싸주는 것이 좋다
+                // Surface 로 감싸주는 것이 좋다.
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    PyeongToSquareMeter()
+                    ConstraintLayoutEx(cardItem)
                 }
             }
         }
     }
-}
 
-@Composable
-fun PyeongToSquareMeter() {
-    // State Hoisting
-    var pyeong by rememberSaveable { mutableStateOf("23") }
-    var squaremeter by rememberSaveable { mutableStateOf((23 * 3.306).toString()) }
-    PyeongToSquareMeterStateless(pyeong = pyeong, squareMeter = squaremeter) {
-        // 후행 람다로 빼기
-        if(it.isBlank()) {
-            pyeong = ""
-            squaremeter = ""
-            return@PyeongToSquareMeterStateless
-        } else {
-            val numericValue = it.toFloatOrNull() ?: return@PyeongToSquareMeterStateless
-            pyeong = numericValue.toString()
-            squaremeter = (numericValue*3.306).toString()
-        }
+    companion object {
+        val cardItem = CardItem(
+            R.drawable.a2,
+            "엔텔로프 캐년",
+            "Dalinaum",
+            "엔텔로프 캐년은 죽기 전에 꼭 봐야할 정경으로 소개되었습니다."
+        )
     }
 }
 
+data class CardItem(
+    val image: Int,
+    val imageDescription: String,
+    val author: String,
+    val description: String
+)
+
 @Composable
-fun PyeongToSquareMeterStateless(
-    pyeong: String,
-    squareMeter: String,
-    onPyeongChanged: (String) -> Unit // 평이 바뀌었을 때 호출할 콜백 함수
-) {
-    /*
-    상태를 가지고 있지 않아서 상태를 바꾸지 않는다.
-    onPyeongChanged 통해 상위 계층에서 상태를 바꾸도록 위임함
-    해당 함수는 상태에 대해서 알지 못한다.
-     */
-    Column(modifier = Modifier.padding(16.dp)) {
-        OutlinedTextField(
-            value = pyeong,
-            onValueChange = onPyeongChanged,
-            label = {
-                Text("평")
-            }
-        )
-        OutlinedTextField(
-            value = squareMeter,
-            onValueChange = {},
-            label = {
-                Text("제곱미터")
-            }
-        )
+fun ConstraintLayoutEx(cardItem: CardItem) {
+    ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+        val (card, image, author, description) = createRefs()
+        Card(
+            modifier = Modifier
+                .padding(10.dp)
+                .constrainAs(card) {
+                    start.linkTo(parent.start)
+                    top.linkTo(parent.top)
+                },
+            elevation = 8.dp
+        ) {
+            Image(
+                painter = painterResource(id = cardItem.image),
+                contentDescription = cardItem.imageDescription,
+                modifier = Modifier
+                    .size(52.dp)
+                    .clip(CircleShape)
+                    .constrainAs(image) {
+                        start.linkTo(card.start, margin = 12.dp)
+                        top.linkTo(card.top, margin = 12.dp)
+                    },
+                contentScale = ContentScale.Crop
+            )
+            Text(
+                text = cardItem.author,
+                modifier = Modifier.constrainAs(author) {
+                    start.linkTo(image.end, margin = 12.dp)
+                    top.linkTo(image.top)
+                }
+            )
+            Text(
+                text = cardItem.description,
+                modifier = Modifier.constrainAs(description) {
+                    top.linkTo(author.bottom)
+                    start.linkTo(author.start)
+                }
+            )
+        }
     }
 }
 
@@ -86,6 +102,6 @@ fun PyeongToSquareMeterStateless(
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        PyeongToSquareMeter()
+        ConstraintLayoutEx(MainActivity.cardItem)
     }
 }
