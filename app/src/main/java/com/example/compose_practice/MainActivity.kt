67bd841fbd5fe272bbbbdc073ccbaa75da0afd62
SEYOUNG PARK <seyeongb18@gmail.com>
2024-09-30 13:58:20 +0900

    [부수효과2 - LaunchedEffect, DisposableEffect] - LaunchedEffect 는 주로 코루틴을 쓸 때 사용함 - 각각 key 값이 바뀌었을 때 Effect 가 reset 되고, DisposableEffect 같은 경우 onDispose 함수가 호출됨

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.compose_practice
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.*
@@ -11,17 +12,18 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.RadioButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import androidx.lifecycle.LifecycleOwner
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -29,120 +31,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposePracticeTheme {
-                // Surface 로 감싸주는 것이 좋다.
                 Surface(
                     color = MaterialTheme.colors.background,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    AnimationTwoEx()
+                    EffectEx()
                 }
             }
         }
     }
 }
 
-/*
-step1 : 변수 생성
-step2 : 변수를 적용
- */
 @Composable
-fun AnimationTwoEx() {
-    var isNormalMode by remember { mutableStateOf(true) }
-    // label : 문제가 발생했을 때 label 을 확인함. 도구에서 디버깅을 하기 위한 label.
-    // 안붙여도 문법적으로 오류는 없지만 붙이는 것이 좋음
-    val transition = updateTransition(targetState = isNormalMode, label = "Animation's Transition")
-    // State<Color> -> Color (delegated property)
-    val backgroundColor by transition.animateColor(label = "background color animation") { state ->
-        when(state) {
-            true -> Color.White
-            false -> Color.Black
-        }
+fun EffectEx(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current) {
+    val scaffoldState = rememberScaffoldState()
+    LaunchedEffect(scaffoldState.snackbarHostState) {
+        scaffoldState.snackbarHostState.showSnackbar("Launched Effect practicing")
     }
-    val textColor by transition.animateColor(label = "text color animation") { state ->
-        when(state) {
-            true -> Color.Black
-            false -> Color.White
-        }
-    }
-    val alpha by transition.animateFloat(label = "alpha animation") { state ->
-        when(state) {
-            true -> 0.5f
-            false -> 1f
-        }
-    }
-
-    Column(
-        modifier = Modifier
-            .background(backgroundColor)
-            .alpha(alpha)
-    ) {
-        RadioButtonWithText(
-            text = "Normal Mode",
-            selected = isNormalMode,
-            color = textColor
-        ) {
-            isNormalMode = true
-        }
-        RadioButtonWithText(
-            text = "Dark Mode",
-            selected = !isNormalMode,
-            color = textColor
-        ) {
-            isNormalMode = false
-        }
-        Crossfade(targetState = isNormalMode) { state ->
-            when(state) {
-                true -> {
-                    Row {
-                        Box(
-                            modifier = Modifier
-                                .size(20.dp)
-                                .background(Color.Red),
-                        ) {
-                            // slotApi
-                            // 일반적으로 slotApi(lambda block) 를 제공하는 composable 함수는 마지막 parameter 가 content 인 경우가 많다.
-                            Text(text = "1")
-                        }
-                        Box(modifier = Modifier
-                            .size(20.dp)
-                            .background(Color.Magenta)) {
-                            Text(text = "2")
-                        }
-                        Box(modifier = Modifier
-                            .size(20.dp)
-                            .background(Color.Blue)) {
-                            Text(text = "3")
-                        }
-                    }
-                }
-                false -> {
-                    Column {
-                        Box(
-                            modifier = Modifier
-                                .size(20.dp)
-                                .background(Color.Red),
-                        ) {
-                            // slotApi
-                            // 일반적으로 slotApi(lambda block) 를 제공하는 composable 함수는 마지막 parameter 가 content 인 경우가 많다.
-                            Text(text = "A")
-                        }
-                        Box(modifier = Modifier
-                            .size(20.dp)
-                            .background(Color.Magenta)) {
-                            Text(text = "B")
-                        }
-                        Box(modifier = Modifier
-                            .size(20.dp)
-                            .background(Color.Blue)) {
-                            Text(text = "C")
-                        }
-                        Surface(modifier = Modifier.background(Color.White)) {
-                            Text(text = "Text 를 Surface 로 감싸주기")
-                        }
-                    }
-                }
+    DisposableEffect(lifecycleOwner) {
+        val observer = LifecycleEventObserver { _, event ->
+            // SAM(Single Abstract Method) 방식
+            // 단일 abstract function 을 가진 interface(LifecycleEventObserver)는
+            // 람다 형태로 추상 함수의 매개변수를 받아올 수 있다.
+            when(event) {
+                Lifecycle.Event.ON_CREATE -> Log.d("DisposableEffect", "ON_CREATE")
+                Lifecycle.Event.ON_START -> Log.d("DisposableEffect", "ON_START")
+                Lifecycle.Event.ON_STOP -> Log.d("DisposableEffect", "ON_STOP")
+                Lifecycle.Event.ON_PAUSE -> Log.d("DisposableEffect", "ON_PAUSE")
+                Lifecycle.Event.ON_RESUME -> Log.d("DisposableEffect", "ON_RESUME")
+                Lifecycle.Event.ON_DESTROY -> Log.d("DisposableEffect", "ON_DESTROY")
+                else -> Log.d("DisposableEffect", "ELSE")
             }
         }
+        // 할당
+        lifecycleOwner.lifecycle.addObserver(observer)
+        onDispose {
+            // 해제
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    }
+    Scaffold(
+        scaffoldState = scaffoldState
+    ) {
+
     }
 }
 
@@ -150,42 +81,7 @@ fun AnimationTwoEx() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        AnimationTwoEx()
+        EffectEx()
     }
 }
 
-@Composable
-fun RadioButtonWithText(
-    text: String,
-    color: Color = Color.Black, // color 값을 설정하지 않았을 때의 기본값
-    selected: Boolean,
-    onClick: () -> Unit
-    // content 는 받지 않는다.
-) {
-    // Row 에 selectable 을 설정했다고 RadioButton 에 속성을 설정하지 않으면 안된다. (작동하지 않는다.)
-    // 즉, RadioButton 은 필수적으로 세팅해야하고, Row 는 클릭 설정을 세팅해도 되고 안해도 된다.
-    Row(
-        modifier = Modifier.selectable(
-            selected = selected,
-            onClick = onClick
-        ),
-        verticalAlignment = Alignment.CenterVertically // row 안에 들어가는 요소에 대한 레이아웃 배치
-    ) {
-        RadioButton(selected = selected, onClick = onClick)
-        Text(
-            text = text,
-            color = color
-        )
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun RadioButtonWithTextPreview() {
-    RadioButtonWithText(
-        text = "Radio Button",
-        color = Color.Red,
-        selected = true,
-        onClick = { }
-    )
-}
