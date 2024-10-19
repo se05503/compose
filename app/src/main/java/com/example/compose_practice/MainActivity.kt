9c6411cee1cc45d4f9a7bc8b8438e8454b792bda
SEYOUNG PARK <seyeongb18@gmail.com>
2024-10-19 21:44:12 +0900

    [CompositionLocalProvider] ①LocalContentAlpha, ②LocalContentColor, ③LocalContentColor.current, ④LocalContentAlpha.current
    - 암시적인 데이터. 명시적으로는parameter 를 통해서 데이터를 넘겨주지만, 해당 방법은 parameter 없이 데이터를 넘기는 방법이다.
    - ctrl + alt + m : 함수 분리 단축키

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
@@ -18,13 +19,14 @@ import com.example.compose_practice.ui.theme.ComposePracticeTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        codeCacheDir.setReadOnly()
         setContent {
             ComposePracticeTheme {
                 Surface(
                     color = MaterialTheme.colors.background,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    ToDoListEx()
+                    CompositionLocalEx()
                 }
             }
         }
@@ -35,174 +37,50 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        ToDoListEx()
-    }
-}
-
-val todoList = ArrayList<String>()
-var isInputButtonClicked by mutableStateOf(false)
-var isEditing by mutableStateOf(false)
-var todoContent by mutableStateOf("")
-
-@Composable
-fun ToDoListEx() {
-    Column {
-        val todoThing = ToDoInputItem()
-        if(isInputButtonClicked) {
-            // 할 일 등록
-            if(todoThing.isBlank()) {
-                val scaffoldState = rememberScaffoldState()
-                LaunchedEffect(scaffoldState.snackbarHostState) {
-                    scaffoldState.snackbarHostState.showSnackbar("내용을 입력해주세요!")
-                }
-                Scaffold(
-                    scaffoldState = scaffoldState
-                ) {
-                    // 얘 넣으니까 "코드를 저렇게 넣었는데 왜 안깜빡이지?" 에 대한 의문점이 풀림 (다시 예상한대로 동작)
-                    // 근데 얘를 왜 넣는거?
-                }
-                isInputButtonClicked = false
-            } else {
-                registerToDoList(todoThing) // 아쉬운 점: 버튼을 누를 때마다 새로 추가된 리스트 뿐만이 아니라 처음 할 일 리스트부터 다시 ui 를 그림
-                isInputButtonClicked = false // recomposition 을 발생시키기 위해 값을 다시 바꿈
-                todoContent = ""
-            }
-        } else {
-            // 동작은 하지만 매우 마음에 들지 않는 코드, 하지만 이렇게 하지 않으면 UI 가 사라진다.
-            LazyColumn {
-                items(todoList) { todo ->
-                    ToDoItem(todo)
-                }
-            }
-        }
+        CompositionLocalEx()
     }
 }
 
 
 @Composable
-fun registerToDoList(todoThing: String) {
-    todoList.add(todoThing)
-    LazyColumn {
-        items(todoList) { todo ->
-            ToDoItem(todo)
-        }
-    }
-}
-
-@Composable
-fun ToDoInputItem(): String {
-    // 입력하기 전 단일 List UI
+fun CompositionLocalEx() {
+    // CompositionLocalProvider 를 이용하면 람다 내에 암시적 값을 설정할 수 있다.
+    // LocalContentAlpha 를 ContentAlpha.disabled 로 설정하겠다.
     Card(
-        elevation = 5.dp,
-        modifier = Modifier.fillMaxWidth()
+        modifier = Modifier.padding(16.dp)
     ) {
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.padding(10.dp)
-        ) {
-            OutlinedTextField(
-                value = todoContent,
-                onValueChange = {
-                    todoContent = it
-                },
-                label = {
-                    Text(text = "What's your plan?")
-                }
-            )
-            Spacer(modifier = Modifier.size(8.dp))
-            Button(onClick = {
-                isInputButtonClicked = true
-            }) {
-                Text(
-                    text = "입력"
-                )
-            }
-        }
-    }
-    return todoContent
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ToDoInputPreview() {
-    ToDoInputItem()
-}
-
-@Composable
-fun ToDoItem(todo: String) {
-    // 입력한 후 단일 List UI
-    var isFinished by remember { mutableStateOf(false) }
-
-    Crossfade(targetState = isEditing) { status ->
-        when (status) {
-            true -> {
-                Card(
-                    elevation = 5.dp,
-                    modifier = Modifier.fillMaxWidth()
-                ) {
-                    Row(
-                        verticalAlignment = Alignment.CenterVertically,
-                        modifier = Modifier.padding(10.dp)
-                    ) {
-                        OutlinedTextField(
-                            value = todoContent,
-                            onValueChange = {
-                                todoContent = it
-                            },
-                            label = {
-                                Text(text = "What's your plan?")
-                            }
-                        )
-                        Spacer(modifier = Modifier.size(8.dp))
-                        Button(onClick = {
-                            isInputButtonClicked = true
-                        }) {
-                            Text(
-                                text = "입력"
-                            )
-                        }
-                    }
-                }
-            }
-            false -> {
-                Card(
-                    elevation = 5.dp,
-                    modifier = Modifier.padding(5.dp) // padding 을 넣어줘야 elevation 효과를 확인할 수 있다.
-                ) {
-                    Row(
-                        modifier = Modifier
-                            .padding(10.dp)
-                            .fillMaxWidth(),
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Text(
-                            text = todo,
-                            modifier = Modifier.weight(1f)
-                        )
-                        Text(text = "완료")
-                        Checkbox(checked = isFinished, onCheckedChange = {
-                            isFinished = !isFinished
-                        })
-                        Button(onClick = { isEditing = true }) {
-                            Text("수정")
-                        }
-                        Spacer(modifier = Modifier.size(8.dp))
-                        Button(onClick = {
-                            // 리스트에서 빼야함
-                        }) {
-                            Text("삭제")
-                        }
-                    }
-                }
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
+            // disabled, medium, high 로 갈수록 색이 진해진다.
+            // 중첩적인 사용이 가능하다.
+            Column(
+                modifier = Modifier.padding(16.dp)
+            ) {
+                test()
+                Text("CompositionLocalProvider 실습중")
+                Text("${LocalContentAlpha.current}")
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun ToDoItemPreview() {
-    ToDoItem("점심 약속")
+private fun test() {
+    Text("CompositionLocalProvider 실습중")
+    Text("CompositionLocalProvider 실습중")
+    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+        CompositionLocalProvider(LocalContentColor provides Color.Blue) {
+            Text("CompositionLocalProvider 실습중")
+            Text("CompositionLocalProvider 실습중")
+            Text("${LocalContentColor.current}") // Color(R,G,B,alpha, ...)
+            Text("${LocalContentAlpha.current}")
+        }
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
+            Text("CompositionLocalProvider 실습중")
+        }
+        Text("CompositionLocalProvider 실습중")
+    }
 }
+
+
 
 
