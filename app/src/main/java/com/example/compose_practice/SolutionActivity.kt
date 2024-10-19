e42d9f68ee98922adaad5e7fa2925379a71f2e29
SEYOUNG PARK <seyeongb18@gmail.com>
2024-10-19 21:03:37 +0900

    [To-do 앱 - LiveData 연동] - livedata 와 state 를 연동하는 예제 - mutableStateListOf -> List 의 값이 추가, 삭제, 변경할 때 ui 가 갱신된다. 하지만, 각 항목의 필드가 바뀌었을 때는 갱신이 되지 않는다. - livedata -> List 가 통째로 다른 List 로 바뀌었을 때만 status가 갱신된다. 좋은 방식은 아니다.
    리스트의 복사본을 만드는 방법
    1. _toDoList.value = ArrayList(_rawToDoList)
    2. _toDoList.value = _rawToDoList.toMutableList()
    → toMutableList 는 새로운 리스트를 return 한다.
    3. mutableListOf<ToDoItem>().also {
      it.addAll(_rawToDoList)
    }
    → addAll 은 파라미터로 들어온 리스트의 모든 항목들을 복사해서 새 리스트를 만든다. addAll 이 속도 성능이 가장 좋다. 단, addAll 을 사용할 때는 shallow copy 를 염두해야 한다.
    
    *shallow copy : 객체(레퍼런스)의 경우에는 문제 없지만, 리스트를 다른 리스트로 복제했을 때 같은 데이터를 가리킬 가능성이 있음.

--- app/src/main/java/com/example/compose_practice/SolutionActivity.kt
+++ app/src/main/java/com/example/compose_practice/SolutionActivity.kt
@@ -1,4 +1,4 @@
-package com.example.compose_practice.ui.theme
+package com.example.compose_practice
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -9,12 +9,15 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.compose_practice.ui.theme.ComposePracticeTheme
 
 class SolutionActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,62 +36,68 @@ class SolutionActivity : ComponentActivity() {
 }
 
 class ToDoViewModel : ViewModel() {
-    // viewmodel 의 생명주기와 composable 의 생명주기는 서로 다르다. -> remember 를 사용하지 못한다.
-    // (이전) 함수 내의 지역 변수 → (이후) 클래스 내에서는 프로퍼티로 작용
-    // 프로퍼티로는 destruction 이 불가능하다.
-    // by 를 통한 상태 위임(delegate)이 가능하다.
-    val text = mutableStateOf("")
+    // 근데 왜 이런 형식으로 하는걸까?
+    private val _text = MutableLiveData("") // 이전) mutableStateOf("") | private 으로 바꿈으로써 외부에서 접근하지 못하도록 함. 같은 class 내에서는 접근 가능
+    val text: LiveData<String> = _text // 외부에서는 _text 가 아닌 text 로 접근함
 
-    // 해당 리스트에 항목이 추가, 삭제, 변경(값이 변경되는 것이 아닌 항목 자체가 변경되는 경우) 될 경우
-    // ui 가 refresh 된다. 이외의 경우(ex. 항목의 값이 바뀌는 경우)에는 refresh 되지 않는다.
-    val toDoList = mutableStateListOf<ToDoItem>()
+    val setText: (String) -> Unit = {
+        _text.value = it
+    }
+
+    // toDoList 를 live data 로 변경한다.
+    // Live data 는 value 가 바뀌어야만 state 가 바뀐다. list 의 일부만 수정해서는 update 가 되지 않는다. 따라서, list 전체를 복제해서 다시 전달해야한다.
+    // 따라서, 좋은 방식은 아니다.
+    private val _rawToDoList = mutableStateListOf<ToDoItem>() // mutableStateListOf -> 항목의 필드가 업데이트 되는 경우에는 업데이트 되지 않는다.
+    private val _toDoList = MutableLiveData<List<ToDoItem>>(_rawToDoList)
+    val toDoList: LiveData<List<ToDoItem>> = _toDoList // 밖에서는 해당 변수를 사용하면서 상태로 바꿔줘야 한다.
+
 
     val onSubmit: (String) -> Unit = {
-        // 리스트에 데이터 넣기
-        val key = (toDoList.lastOrNull()?.key ?: 0) + 1
-        toDoList.add(ToDoItem(key, it))
-        text.value = ""
+        val key = (_rawToDoList.lastOrNull()?.key ?: 0) + 1
+        _rawToDoList.add(ToDoItem(key, it))
+        _toDoList.value = ArrayList(_rawToDoList)
+        _text.value = ""
     }
 
     val onToggle: (key: Int, checked: Boolean) -> Unit = { key, checked ->
         // key 를 통해서 index 찾기
         // toDoList[i].isFinished = checked 가 아닌 copy 를 하는 이유?
         // 목적: UI refresh 를 위함 (항목의 값이 아닌 항목 자체를 바꿔야 ui 가 갱신된다)
-        val i = toDoList.indexOfFirst { it.key == key }
-        toDoList[i] = toDoList[i].copy(isFinished = checked)
+        val i = _rawToDoList.indexOfFirst { it.key == key }
+        _rawToDoList[i] = _rawToDoList[i].copy(isFinished = checked)
+        _toDoList.value = _rawToDoList.toMutableList()
     }
 
     val onDelete: (key: Int) -> Unit = { key ->
-        val i = toDoList.indexOfFirst { it.key == key }
-        toDoList.removeAt(i) // 원소를 삭제하면 UI 가 갱신된다.
+        val i = _rawToDoList.indexOfFirst { it.key == key }
+        _rawToDoList.removeAt(i) // 원소를 삭제하면 UI 가 갱신된다.
+        _toDoList.value = mutableListOf<ToDoItem>().also {
+            it.addAll(_rawToDoList)
+        }
     }
 
     val onEdit: (key: Int, newText: String) -> Unit = { key, newText ->
-        val i = toDoList.indexOfFirst { it.key == key }
-        toDoList[i] = toDoList[i].copy(text = newText)
-    }
-}
-
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposePracticeTheme {
-        TopLevel()
+        val i = _rawToDoList.indexOfFirst { it.key == key }
+        _rawToDoList[i] = _rawToDoList[i].copy(text = newText)
+        _toDoList.value = mutableListOf<ToDoItem>().also {
+            it.addAll(_rawToDoList)
+        }
     }
 }
 
 // 기본 값 = viewModel()
 @Composable
 fun TopLevel(viewModel: ToDoViewModel = viewModel()) {
-
     Scaffold {
         Column {
-            ToDoInput(text = viewModel.text.value, onTextChanged = { viewModel.text.value = it }, onSubmit = viewModel.onSubmit)
+            // live data 가 update 될 때마다 갱신이 되려면 live data 를 state  로 바꿔야한다.
+            // observeAsState 는 기본적으로 null 이 발생할 수 있어서, null 이 발생하지 않으려면 기본값을 설정해야 한다.
+            ToDoInput(text = viewModel.text.observeAsState("").value, onTextChanged = viewModel.setText, onSubmit = viewModel.onSubmit)
+            val toDoItems = viewModel.toDoList.observeAsState(emptyList()).value
             LazyColumn {
                 // 어떤 항목이 재사용 가능한지 아닌지는 key 세팅을 통해 판단할 수 있다 (jetpack compose)
                 // key 가 같으면 재사용 할 수 있다고 판단 -> 효과적인 렌더링 가능
-                items(viewModel.toDoList, key = { it.key }) { todoItem ->
+                items(toDoItems, key = { it.key }) { todoItem ->
                     ToDoOutput(
                         toDoItem = todoItem,
                         onToggle = viewModel.onToggle,
