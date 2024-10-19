67113bbb62a55e33963eb8a11b7479d5b55215cb
SEYOUNG PARK <seyeongb18@gmail.com>
2024-10-19 22:20:48 +0900

    [Theme] surface = 배경 색상 onSurface = 전경 색상
    primary가 있으면 onPrimary 가 있듯이 짝을 이룬다.
    
    LightColorPalette 와 마찬가지로 다크모드에서는 DarColorPalette 를 사용한다.
    
    primary(버튼 배경), onPrimary(버튼 전경 = 텍스트 색상)
    
    자신만의 브랜드 디자인을 만들때 해당 개념을 사용할 수 있다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,15 +3,10 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.ui.theme.ComposePracticeTheme
@@ -26,7 +21,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    CompositionLocalEx()
+                    ThemeEx()
                 }
             }
         }
@@ -37,52 +32,29 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        CompositionLocalEx()
+        ThemeEx()
     }
 }
 
-// CompositionLocalProvider 에서 사용할 수 있다.
-val LocalElevation = compositionLocalOf { 8.dp }
-
 @Composable
-fun CompositionLocalEx() {
-
-    // 기본값은 8.dp 이지만 12.dp 로 override 함
-    CompositionLocalProvider(LocalElevation provides 12.dp) {
-        Card(
-            modifier = Modifier.padding(16.dp),
-            elevation = LocalElevation.current
-        ) {
-            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.disabled) {
-                Column(
-                    modifier = Modifier.padding(16.dp)
-                ) {
-                    test()
-                    Text("CompositionLocalProvider 실습중")
-                    Text("${LocalContentAlpha.current}")
-                }
+fun ThemeEx() {
+    Card(
+        modifier = Modifier.padding(16.dp)
+    ) {
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text("Theme 공부중")
+            Text("Theme 공부중")
+            Text("Theme 공부중")
+            Text("Theme 공부중")
+            Text("Theme 공부중")
+            Text("Theme 공부중")
+            Button(onClick = { /*TODO*/ }) {
+                Text("버튼")
             }
         }
     }
 }
 
-@Composable
-private fun test() {
-    Text("CompositionLocalProvider 실습중")
-    Text("CompositionLocalProvider 실습중")
-    CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-        CompositionLocalProvider(LocalContentColor provides Color.Blue) {
-            Text("CompositionLocalProvider 실습중")
-            Text("CompositionLocalProvider 실습중")
-            Text("${LocalContentColor.current}") // Color(R,G,B,alpha, ...)
-            Text("${LocalContentAlpha.current}")
-        }
-        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.high) {
-            Text("CompositionLocalProvider 실습중")
-        }
-        Text("CompositionLocalProvider 실습중")
-    }
-}
 
 
 
