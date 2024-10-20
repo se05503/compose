f779e88185c3427a96a650ce83676a1a9b75e367
SEYOUNG PARK <seyeongb18@gmail.com>
2024-10-20 16:40:45 +0900

    [포켓몬 앱 - 스스로 해보기] 일단 dummy data 활용해서 1차 틀 만들기 navigation 을 활용해봤다! 기존에는 ui 모양이 다르면 composable 함수로 분리했었는데, navigation 을 도입하니까 함수로 분리할 필요성을 못느꼈다.
    의문점)
    - 패키지명이 잘못된 것 같다. ui.theme.ui.theme 이 아니라 ui.theme 이어야 하는 것 같은데 잘 모르겠다.
    - crossfade vs navigation 차이?
    다른 화면으로 이동해야하니 navigation 을 사용해보자!
    - Arragement vs Alignment 차이?

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.compose_practice
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.navigation.NavController
-import androidx.navigation.NavHost
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
-import com.example.compose_practice.ui.theme.ComposePracticeTheme
+import com.example.compose_practice.ui.theme.ui.theme.ComposePracticeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +32,7 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colors.background,
                     modifier = Modifier.fillMaxSize()
                 ) {
-                    navigationEx()
+                    PokemonEx()
                 }
             }
         }
@@ -37,87 +43,84 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun DefaultPreview() {
     ComposePracticeTheme {
-        navigationEx()
+        PokemonEx()
     }
 }
 
 @Composable
-fun navigationEx(
-    modifier: Modifier = Modifier,
-    navController: NavHostController = rememberNavController() // = 다음은 기본값
-) {
-    NavHost(navController = navController, startDestination = "Home", modifier = modifier) {
-        composable("Home") {
-            Column {
-                Text("Home 화면입니다.")
-                Button(onClick = {navController.navigate("Office") {
-                    popUpTo("Home") {
-                        inclusive = true
-                    }
-                } }) {
-                    Text("Office 로 이동하기")
-                }
-                Button(onClick = {navController.navigate("Playground") {
-                    popUpTo("Home") {
-                        inclusive = true
-                    }
-                } }) {
-                    Text("Playground 로 이동하기")
-                }
-                Button(onClick = { navController.navigate("Home") {
-                    launchSingleTop = true
-                } }) {
-                    Text("Home 로 이동하기")
-                }
-                Button(onClick = { navController.navigate("Data/parkseyoung") }) { // 슬래시 뒤에 전달할 데이터를 작성한다.
-                    Text("Data 전달 화면으로 이동")
-                }
-            }
-        }
-        composable("Office") {
-            Column {
-                Text("Office 화면입니다.")
-                Button(onClick = {navController.navigate("Home") {
-                    popUpTo("Home") {
-                        inclusive = true
-                    }
-                } }) {
-                    Text("Home 로 이동하기")
-                }
-                Button(onClick = {navController.navigate("Playground") {
-                    popUpTo("Home") {
-                        inclusive = true
-                    }
-                } }) {
-                    Text("Playground 로 이동하기")
-                }
-            }
-        }
-        composable("Playground") {
-            Column {
-                Text("Playground 화면입니다.")
-                Button(onClick = {navController.navigate("Home") {
-                    popUpTo("Home") {
-                        inclusive = true
-                    }
-                } }) {
-                    Text("Home 로 이동하기")
-                }
-                Button(onClick = {navController.navigate("Office") {
-                    popUpTo("Home") {
-                        inclusive = true
-                    }
-                } }) {
-                    Text("Office 로 이동하기")
-                }
-            }
-        }
-        composable("Data/{username}") { // username = 전달받는 데이터의 key 값으로, 전달 받을 때는 중괄호를 쳐야한다.
-            val receivedData = it.arguments?.getString("username")
-            Text("전달받은 데이터: $receivedData")
+fun PokemonEx() {
+    val dummyPokemons = listOf(
+        Pokemon(0, "포켓몬1", "http1", R.drawable.pokemon1),
+        Pokemon(1, "포켓몬2", "http2", R.drawable.pokemon2),
+        Pokemon(2, "포켓몬3", "http3", R.drawable.pokemon3)
+    )
+    LazyColumn {
+        items(dummyPokemons) { dummyPokemon ->
+            IndividualPokemonItem(dummyPokemon)
         }
     }
 }
+
+@Composable
+fun IndividualPokemonItem(
+    pokemon: Pokemon,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(navController = navController, startDestination = "Home") {
+        composable("Home") {
+            Card(
+                modifier = Modifier
+                    .padding(8.dp)
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    modifier = Modifier.padding(8.dp)
+                ) {
+                    Column(
+                        modifier = Modifier.weight(1f)
+                    ) {
+                        Text(
+                            text = "포켓몬: ${pokemon.pokemonName}",
+                            fontWeight = FontWeight.Bold
+                        )
+                        Text(
+                            text = pokemon.pokemonAddress,
+                            color = Color.Gray
+                        )
+                    }
+                    Button(onClick = {
+                        navController.navigate("Detail")
+                    }) {
+                        Text(text = "보기")
+                    }
+                }
+            }
+        }
+        composable("Detail") {
+            Card(modifier = Modifier.padding(8.dp)) {
+                Column(
+                    modifier = Modifier.padding(12.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(text = pokemon.pokemonName)
+                    Image(painter = painterResource(id = pokemon.pokemonImage), contentDescription = pokemon.pokemonName)
+                    Button(onClick = {
+                        navController.navigate("Home")
+                    }) {
+                        Text(text ="뒤로")
+                    }
+                }
+            }
+        }
+    }
+}
+
+data class Pokemon(
+    val id: Int,
+    val pokemonName: String,
+    val pokemonAddress: String,
+    val pokemonImage: Int // server 연결할 때 type 을 String 으로 바꾸기
+)
 
 
 
