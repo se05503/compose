0c97c5d73ca9c60133198d75161e4f6609d88857
SEYOUNG PARK <seyeongb18@gmail.com>
2024-10-23 19:29:19 +0900

    [포켓몬 앱] - 포켓몬 이미지 받아오고 뷰에 성공적으로 띄우기 - 마지막 서버 함수 구현 - query vs path 차이 구분하기 - 오류 1) AsyncImage 에 전달되는 image 값이 null 이다. -> image 는 기존에 String 이었다. @Composable 인 Surface 가 recomposition 이 일어나려면 상태(status)가 바뀌어야 한다. 따라서, 이미지 변수를 상태로 바꾸고, 서버로부터 값을 받아와 변경되었을때, recomposition 으로 ui 가 갱신되도록 하여 문제를 해결하였다.
    - 오류 2) navigation의 인자로 url 을 넘겨줄려면, 넘겨줄 때 encoding, 넘겨받은 후 decoding 을 처리해야 한다. 그리고 되도록이면 url 을 넘기지 않는 것이 좋다.

--- app/src/main/java/com/example/compose_practice/MainActivity.kt
+++ app/src/main/java/com/example/compose_practice/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.compose_practice
 
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
@@ -10,7 +11,7 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -32,7 +33,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : ComponentActivity() {
 
-    var pokemonItems = listOf<PokemonEntity>()
+    lateinit var pokemonItems: List<PokemonEntity>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -53,7 +54,7 @@ class MainActivity : ComponentActivity() {
                 response: Response<PokemonResponse>
             ) {
                 if (response.isSuccessful) {
-                    Toast.makeText(this@MainActivity, response.message(), Toast.LENGTH_SHORT).show()
+                    // response message 가 아무것도 오지 않는다.
                     val response = response.body()
                     pokemonItems = response?.results ?: emptyList()
                     setContent {
@@ -102,9 +103,8 @@ fun PokemonEx(pokemonItems: List<PokemonEntity>) {
 @Composable
 fun IndividualPokemonItem(
     pokemon: PokemonEntity,
-    navController: NavHostController = rememberNavController()
+    navController: NavHostController = rememberNavController(),
 ) {
-    val pid = pokemon.url.filter { it.isDigit() }.removePrefix("2") // 이렇게 받아오는건 아닌 것 같은데.. position 도 안되는 것 같고.. 다른 방법이 있을까?
     NavHost(navController = navController, startDestination = "Home") {
         composable("Home") {
             Card(
@@ -128,36 +128,39 @@ fun IndividualPokemonItem(
                         )
                     }
                     Button(onClick = {
-                        navController.navigate("Detail/$pid")
+                        val encodedUrl = Uri.encode(pokemon.url)
+                        navController.navigate("Detail/$encodedUrl")
                     }) {
                         Text(text = "보기")
                     }
                 }
             }
         }
-
-        composable("Detail/{pid}") {
-            var image: String? = null
-            val pid = it.arguments?.getString("pid")?.toInt()
+        composable("Detail/{encodedUrl}") {
+            var image by remember { mutableStateOf("") }
+            val decodedUrl = Uri.decode(it.arguments?.getString("encodedUrl"))
+            val pid = decodedUrl?.filter { url ->
+                url.isDigit()
+            }?.removePrefix("2")?.toInt() // 이렇게 받아오는건 아닌 것 같은데.. position 도 안되는 것 같고.. 다른 방법이 있을까?
             val retrofit = Retrofit.Builder()
                 .baseUrl("https://pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             val networkService = retrofit.create(NetworkService::class.java)
-            networkService.getPokemonImages(pid!!).enqueue(object : Callback<PokemonSprites> {
+            networkService.getPokemonImages(pid ?: 0).enqueue(object : Callback<PokemonSprites> {
                 override fun onResponse(
                     call: Call<PokemonSprites>,
                     response: Response<PokemonSprites>
                 ) {
-                    if(response.isSuccessful) {
+                    if (response.isSuccessful) {
                         // 여기에 걸린다. 이미지 잘온다.
                         val response = response.body()
-                        image = response?.sprites?.front_default // 여기는 이미지가 잘 오는데
+                        image = response!!.sprites.front_default // 여기는 이미지가 잘 오는데
                     }
                 }
 
                 override fun onFailure(call: Call<PokemonSprites>, t: Throwable) {
-                    Log.d("response: ","onFailure")
+                    Log.d("response: ", "onFailure")
                 }
             })
             Surface(
@@ -169,7 +172,11 @@ fun IndividualPokemonItem(
                         horizontalAlignment = Alignment.CenterHorizontally
                     ) {
                         Text(text = pokemon.name)
-                        AsyncImage(model = image, contentDescription = pokemon.name) // 여기서는 이미지가 null 로 전달되네
+                        AsyncImage(
+                            model = image,
+                            contentDescription = pokemon.name,
+                            modifier = Modifier.size(100.dp)
+                        ) // 여기서는 이미지가 null 로 전달되네
                         Button(onClick = {
                             navController.navigate("Home")
                         }) {
