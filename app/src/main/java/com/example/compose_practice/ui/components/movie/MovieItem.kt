e2f32ae24258aacc3d4e4489e1c604e1c2d038ae
se05503 <seyeongb18@gmail.com>
2025-08-16 16:33:00 +0900

    [feat] 영화 썸네일 만들기 - 중복되는 값 상수로 빼기 - Text의 overflow = TextOverflow.Ellipsis → 끝 부분 생략

---
+++ app/src/main/java/com/example/compose_practice/ui/components/movie/MovieItem.kt
@@ -0,0 +1,70 @@
+package com.example.compose_practice.ui.components.movie
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+// 값이 중복되면 상수로 빼는게 좋다. 상수는 BIG_SNAKE_CASE 를 쓰는게 좋다.
+private val CARD_WIDTH = 150.dp
+
+// class가 아니라 composable 함수로 만듬
+@Composable
+fun MovieItem() {
+    Column(
+        modifier = Modifier
+            .width(CARD_WIDTH)
+            .padding(10.dp)
+    ) {
+        Poster(
+            modifier = Modifier.width(CARD_WIDTH)
+        )
+        Text(
+            text = "영화 제목입니다. 영화 제목입니다.",
+            maxLines = 1,
+            overflow = TextOverflow.Ellipsis, // 글자의 길이가 길때 끝에 ... 표시
+            modifier = Modifier.padding(top = 12.dp)
+        )
+        Row(
+            modifier = Modifier.padding(vertical = 8.dp),
+            verticalAlignment = Alignment.CenterVertically // Alignment = 다른 방향, Arrangement = 같은 방향
+        ) {
+            Text(
+                text = "5.0"
+            )
+        }
+    }
+}
+
+// 따로 컴포넌트로 분리. 해당 부분을 눌러서 상세 정보로 이동하기 때문
+@Composable
+fun Poster(
+    modifier: Modifier
+) {
+    Card( // 모서리 둥글게 함
+        modifier.fillMaxWidth().height(200.dp)
+    ) {
+        Box(
+            modifier = Modifier.background(Color.Blue)
+        )
+    }
+}
+
+@Preview
+@Composable
+fun MovieItemPreview() {
+    MovieItem()
+}
