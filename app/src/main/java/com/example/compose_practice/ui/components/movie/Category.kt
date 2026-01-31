64e0ec293022f2ebee517d69327e77f5efb0cdb4
se05503 <seyeongb18@gmail.com>
2026-01-31 11:49:13 +0900

    [feat] 컴포넌트에 테마 적용하기

--- app/src/main/java/com/example/compose_practice/ui/components/movie/Category.kt
+++ app/src/main/java/com/example/compose_practice/ui/components/movie/Category.kt
@@ -4,11 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.compose_practice.ui.theme.Paddings
 
 @Composable
 fun CategoryRow() {
@@ -39,14 +41,21 @@ fun CategoryRow() {
 
 @Composable
 private fun CategoryTitle(titleName: String) {
+
     Text(
         text = titleName,
-        modifier = Modifier.padding(10.dp)
+        modifier = Modifier.padding(
+            vertical = Paddings.large,
+            horizontal = Paddings.extra
+        ),
+        style = MaterialTheme.typography.h5
     )
 }
 
 @Preview
 @Composable
 fun CategoryRowPreview() {
-    CategoryRow()
+    MaterialTheme {
+        CategoryRow()
+    }
 }
--- app/src/main/java/com/example/compose_practice/ui/components/movie/MovieItem.kt
+++ app/src/main/java/com/example/compose_practice/ui/components/movie/MovieItem.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,13 +23,16 @@ import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.compose_practice.R
+import com.example.compose_practice.ui.theme.Paddings
+
+private val ICON_SIZE = 12.dp
 
 @Composable
 fun MovieItem() {
     Column(
         modifier = Modifier
             .width(150.dp)
-            .padding(10.dp)
+            .padding(Paddings.large)
     ) {
         Poster(
             modifier = Modifier.fillMaxWidth()
@@ -37,20 +41,23 @@ fun MovieItem() {
             text = "영화 제목입니다. 영화 제목입니다.",
             maxLines = 1,
             overflow = TextOverflow.Ellipsis, // 글자의 길이가 길때 끝에 ... 표시
-            modifier = Modifier.padding(top = 12.dp)
+            modifier = Modifier.padding(top = Paddings.large),
+            style = MaterialTheme.typography.body2
         )
         Row(
-            modifier = Modifier.padding(vertical = 8.dp),
+            modifier = Modifier.padding(vertical = Paddings.medium),
             verticalAlignment = Alignment.CenterVertically // Alignment = 다른 방향, Arrangement = 같은 방향
         ) {
             Icon(
-                modifier = Modifier.padding(4.dp).size(12.dp),
+                modifier = Modifier.padding(Paddings.small).size(ICON_SIZE),
                 imageVector = ImageVector.vectorResource(id = R.drawable.ic_star),
                 tint = Color.Black.copy(alpha = 0.5f),
                 contentDescription = "영화에 대한 평점" // 시각 장애인을 위한 청각 서비스를 제공할 때 읽어줌. 아이콘, 이미지에 무조건 넣어줘야함
             )
             Text(
-                text = "5.0"
+                text = "5.0",
+                style = MaterialTheme.typography.body2,
+                color = MaterialTheme.colors.onSurface.copy(alpha = 0.5f)
             )
         }
     }
